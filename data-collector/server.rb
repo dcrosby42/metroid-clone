5bd853f502e7b4f64195ae68109f6f2332328d19
David <dcrosby42@gmail.com>
2015-11-11 19:52:19 -0500

    fix

--- data-collector/server.rb
+++ data-collector/server.rb
@@ -2,24 +2,26 @@ require 'sinatra'
 require 'json'
 require 'sinatra/cross_origin'
 
-set :port, 5012
-
-# options '/*' do
-#     response["Access-Control-Allow-Headers"] = "origin, x-requested-with, content-type"
-# end
 configure do
   enable :cross_origin
 end
 
+set :port, 5012
+
+set :allow_origin, :any
+
 post '/capture-data' do
-  puts "hi!"
-  cross_origin
-  obj = JSON.parse(request.body.read)
-  p obj
-  File.open("data.json","a") do |f|
-    obj['data'].each do |x|
-      f.puts x.to_json
+  begin
+    cross_origin
+    obj = JSON.parse(request.body.read)
+    p obj
+    File.open("data.json","a") do |f|
+      obj['data'].each do |x|
+        f.puts x.to_json
+      end
     end
+    {status: "success"}.to_json
+  rescue Exception => e
+    puts "#{e.message}: #{e.backtrace.join("\n")}"
   end
-  {status: "success"}.to_json
 end
